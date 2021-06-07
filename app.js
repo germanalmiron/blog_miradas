ceb6180a0485c8f2f73a818c4d4faa51349ba2fa
Germán Almirón <>
2021-06-06 22:12:23 -0300

    blog miradas

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Configuración inicial
 const express = require("express");
 const app = express();
+const path = require('path')
+var router = express.Router();
 
-// rutas
+// setting
+app.set('views', path.join(__dirname, 'public'));
+
+//static files
 app.use(express.static('public'));
 
 // Iniciar servidor
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor escuchando en el puerto 3000');
 });
-
-
--- public/index.php
+++ public/index.php
@@ -1,2 +1,4 @@
 <?php require_once("index.html")?>
-
+<?php require_once("miradas.html")?>
+<?php require_once("contacto.html")?>
+<?php require_once("mundo.html")?>
